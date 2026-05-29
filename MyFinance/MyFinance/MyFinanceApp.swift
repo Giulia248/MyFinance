4bdaeff27fdb0629c36599b3bd74efae9ebe78a1
Giulia Floris <giulia.floris@accenture.com>
2026-05-29 21:03:57 +0200

    initial 2

---
+++ MyFinance/MyFinance/MyFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFinanceApp.swift
+//  MyFinance
+//
+//  Created by giulia.floris on 29/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
